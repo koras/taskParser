762579ae939d1578be55d2078b77cde712898330
koras <kos6476@yandex.ru>
2019-09-14 01:14:37 +0300

    first commit

---
+++ parser.cpp
@@ -0,0 +1,18 @@
+/**
+* only linux
+* g++ -o parser parser.cpp -lcurl
+*
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <curl/curl.h>
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+ 
+ 
+  return 0;
+}
