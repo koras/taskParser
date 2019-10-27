0c8f5146bd2c13daec116cbd35b88533132d7b68
koras <kos6476@yandex.ru>
2019-10-27 21:55:48 +0300

    sad

--- parser.cpp
+++ parser.cpp
@@ -39,18 +39,9 @@ int main(int argc, char* argv[])
 {
  
  string name, family;
-
-   //https://github.com/a-ishere/a-ishere.github.io
-   //https://github.com/a-ishere/a-ishere.github.io/blob/master/index.html
+ 
   const   string  errorDomen = "Domen github not found"; 
-  const   string  startSearch = "Start search github \n"; 
- // const  char* urlPathGit = "https://github.com/a-ishere/a-ishere.github.io";
- //const  char* urlPathGit = "https://raw.githubusercontent.com/a-ishere/a-ishere.github.io/master/index.html";
-
-  // char  urlPathGit[150] = "https://github.com/Ekaterina1618/Ekaterina1618.github.io";
-         //       https://github.com/Ekaterina1618/Ekaterina1618.github.io
- //https://raw.githubusercontent.com/Ekaterina1618/Ekaterina1618.github.io/master/index.html
-
+  const   string  startSearch = "Start search github \n";  
 
  char  urlPathGit[150] = "https://raw.githubusercontent.com/Ekaterina1618/Ekaterina1618.github.io/master/index.html";
 
